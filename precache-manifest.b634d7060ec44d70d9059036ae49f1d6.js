fa03b4b3b99c8a23c06a96f377a09f073fad96b3
Imran Sahil <imdshizzle@gmail.com>
2020-03-01 23:07:17 -0500

    Updates

---
+++ precache-manifest.b634d7060ec44d70d9059036ae49f1d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ef203c93e636370bdc61bee90dfbc76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "109a9baebe1d1561efb0",
+    "url": "/static/css/main.f77b7f27.chunk.css"
+  },
+  {
+    "revision": "ca2449acc6084cc2b7e0",
+    "url": "/static/js/2.ea1381cf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.ea1381cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "109a9baebe1d1561efb0",
+    "url": "/static/js/main.d82f5dd5.chunk.js"
+  },
+  {
+    "revision": "c611f573540fed622b53",
+    "url": "/static/js/runtime-main.2b8ec076.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
